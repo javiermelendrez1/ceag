02f86d5f6bddcd5875b422ed9dcdfb7c31c7e8a4
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2023-01-14 17:37:14 -0800

    Add files via upload

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,1 +1,12 @@
+function scrollValue() {
+    var navbar = document.getElementById('navbar');
+    var scroll = window.scrollY;
+    if (scroll < 200) {
+        navbar.classList.remove('BgColour');
+        console.log('scroll')
+    } else {
+        navbar.classList.add('BgColour');
+    }
+}
 
+window.addEventListener('scroll', scrollValue);
